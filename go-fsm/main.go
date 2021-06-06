1419a3b8f5f320d33c4f9e488d3603ecf6ec64e4
alextanhongpin <alextan220990@gmail.com>
2021-06-06 13:30:14 +0800

    refactor: use types for statuses, shift callbacks out

--- go-fsm/main.go
+++ go-fsm/main.go
@@ -3,162 +3,222 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 
 	"github.com/looplab/fsm"
 )
 
+type StepStatus string
+
+const (
+	StepStatusPending      StepStatus = "pending"
+	StepStatusSuccess      StepStatus = "success"
+	StepStatusFailed       StepStatus = "failed"
+	StepStatusCompensating StepStatus = "compensating"
+	StepStatusCompensated  StepStatus = "compensated"
+)
+
+func (s StepStatus) Valid() bool {
+	switch s {
+	case
+		StepStatusPending,
+		StepStatusSuccess,
+		StepStatusFailed,
+		StepStatusCompensating,
+		StepStatusCompensated:
+		return true
+	default:
+		return false
+	}
+}
+
+func (s StepStatus) String() string {
+	return string(s)
+}
+
+type SagaStatus string
+
+const (
+	SagaStatusPending      SagaStatus = "pending"
+	SagaStatusSuccess      SagaStatus = "success"
+	SagaStatusCompensating SagaStatus = "compensating"
+	SagaStatusDone         SagaStatus = "done"
+)
+
+func (s SagaStatus) Valid() bool {
+	switch s {
+	case
+		SagaStatusPending,
+		SagaStatusSuccess,
+		SagaStatusCompensating,
+		SagaStatusDone:
+		return true
+	default:
+		return false
+	}
+}
+
 type Step struct {
-	Name   string
-	Status string
-	fsm    *fsm.FSM
+	Name   string     `json:"name"`
+	Status StepStatus `json:"status"`
+	FSM    *fsm.FSM   `json:"-"`
 }
 
-func NewCreateBookingStep(o *Orchestrator) *Step {
-	step := &Step{
-		Name:   "create-booking",
-		Status: "pending",
+func NewCreateBookingStep(step *Step, additionalCallbacks fsm.Callbacks) *Step {
+	if step.Name != "create-booking" {
+		return step
 	}
-	step.fsm = fsm.NewFSM(
-		"pending",
+
+	callbacks := fsm.Callbacks{
+		"enter_state": func(e *fsm.Event) {
+			status := StepStatus(e.Dst)
+			if !status.Valid() {
+				log.Fatalln("invalid step status", status)
+			}
+			step.Status = status
+		},
+	}
+
+	for k, v := range additionalCallbacks {
+		_, exists := callbacks[k]
+		if exists {
+			panic("cannot override existing callback")
+		}
+		callbacks[k] = v
+	}
+
+	step.FSM = fsm.NewFSM(
+		StepStatusPending.String(),
 		fsm.Events{
-			{Name: "booking_created", Src: []string{"pending"}, Dst: "success"},
-			{Name: "booking_cancelled", Src: []string{"success"}, Dst: "compensated"},
+			{Name: "booking_created", Src: []string{StepStatusPending.String()}, Dst: StepStatusSuccess.String()},
+			{Name: "booking_cancelled", Src: []string{StepStatusSuccess.String()}, Dst: StepStatusCompensated.String()},
 		},
-		fsm.Callbacks{
-			"enter_state": func(e *fsm.Event) {
-				step.Status = e.Dst
-			},
-			"leave_booking_created": func(e *fsm.Event) {
-				if err := o.CreatePayment(); err != nil {
-					e.Cancel(err)
-				}
-			},
-		},
+		callbacks,
 	)
 	return step
 }
 
-func NewCreatePaymentStep(o *Orchestrator) *Step {
-	step := &Step{
-		Name:   "create-payment",
-		Status: "pending",
+func NewCreatePaymentStep(step *Step, additionalCallbacks fsm.Callbacks) *Step {
+	if step.Name != "create-payment" {
+		return step
 	}
-	step.fsm = fsm.NewFSM(
-		"pending",
+	callbacks := fsm.Callbacks{
+		"enter_state": func(e *fsm.Event) {
+			// On entering any state, update the status of the step.
+			status := StepStatus(e.Dst)
+			if !status.Valid() {
+				log.Fatalln("invalid step status", status)
+			}
+			step.Status = status
+		},
+	}
+
+	for k, v := range additionalCallbacks {
+		_, exists := callbacks[k]
+		if exists {
+			panic("cannot override existing callbacks")
+		}
+		callbacks[k] = v
+	}
+	step.FSM = fsm.NewFSM(
+		StepStatusPending.String(),
 		fsm.Events{
-			{Name: "payment_created", Src: []string{"pending"}, Dst: "success"},
-			{Name: "payment_expired", Src: []string{"pending"}, Dst: "failed"},
-			{Name: "payment_failed", Src: []string{"pending"}, Dst: "failed"},
-			{Name: "payment_refunded", Src: []string{"success"}, Dst: "compensated"},
+			{Name: "payment_created", Src: []string{StepStatusPending.String()}, Dst: StepStatusSuccess.String()},
+			{Name: "payment_expired", Src: []string{StepStatusPending.String()}, Dst: StepStatusFailed.String()},
+			{Name: "payment_failed", Src: []string{StepStatusPending.String()}, Dst: StepStatusFailed.String()},
+			{Name: "payment_refunded", Src: []string{StepStatusSuccess.String()}, Dst: StepStatusCompensated.String()},
 		},
-		fsm.Callbacks{
-			"enter_state": func(e *fsm.Event) {
-				step.Status = e.Dst
-			},
-			"leave_state": func(e *fsm.Event) {
-				switch e.Event {
-				case "payment_created":
-					// Next.
-					if err := o.ConfirmBooking(); err != nil {
-						e.Cancel(err)
-					}
-				default:
-					// Prev.
-					if err := o.CancelBooking(); err != nil {
-						e.Cancel(err)
-					}
-				}
-			},
-		},
+		callbacks,
 	)
 	return step
 }
 
-func NewConfirmBookingStep(o *Orchestrator) *Step {
-	step := &Step{
-		Name:   "confirm-booking",
-		Status: "pending",
+func NewConfirmBookingStep(step *Step, additionalCallbacks fsm.Callbacks) *Step {
+	if step.Name != "confirm-booking" {
+		return step
 	}
-	step.fsm = fsm.NewFSM(
-		"pending",
+
+	callbacks := fsm.Callbacks{
+		"enter_state": func(e *fsm.Event) {
+			status := StepStatus(e.Dst)
+			if !status.Valid() {
+				log.Fatalln("invalid step status", status)
+			}
+			step.Status = status
+		},
+	}
+
+	for k, v := range additionalCallbacks {
+		_, exists := callbacks[k]
+		if exists {
+			panic("cannot override existing callbacks")
+		}
+		callbacks[k] = v
+	}
+
+	step.FSM = fsm.NewFSM(
+		StepStatusPending.String(),
 		fsm.Events{
-			{Name: "booking_confirmed", Src: []string{"pending"}, Dst: "success"},
-			{Name: "booking_failed", Src: []string{"pending"}, Dst: "failed"},
-			{Name: "booking_rejected", Src: []string{"success"}, Dst: "failed"},
+			{Name: "booking_confirmed", Src: []string{StepStatusPending.String()}, Dst: StepStatusSuccess.String()},
+			{Name: "booking_failed", Src: []string{StepStatusPending.String()}, Dst: StepStatusFailed.String()},
+			{Name: "booking_rejected", Src: []string{StepStatusSuccess.String()}, Dst: StepStatusFailed.String()},
 		},
-		fsm.Callbacks{
-			"enter_state": func(e *fsm.Event) {
-				step.Status = e.Dst
-			},
-			"leave_state": func(e *fsm.Event) {
-				if e.Event == "booking_confirmed" {
-					return
-				}
-				if err := o.RefundPayment(); err != nil {
-					e.Cancel(err)
-				}
-			},
-		},
+		callbacks,
 	)
 	return step
 }
 
-type Orchestrator struct {
-	ID      string
-	Name    string
-	Payload []byte
-	Steps   []*Step
-	Version int
+type Saga struct {
+	ID        string        `json:"id"`
+	Name      string        `json:"name"`
+	Payload   []byte        `json:"payload"`
+	Steps     []*Step       `json:"steps"`
+	Version   int           `json:"version"`
+	Callbacks fsm.Callbacks `json:"-"`
 }
 
-func NewBookingOrchestrator(id string) *Orchestrator {
-	o := &Orchestrator{
+func NewBookingSaga(id string, callbacks fsm.Callbacks) *Saga {
+	saga := &Saga{
 		ID:      id,
 		Name:    "create-booking-saga",
 		Version: 1,
+		Steps: []*Step{
+			&Step{Name: "create-booking", Status: StepStatusPending},
+			&Step{Name: "create-payment", Status: StepStatusPending},
+			&Step{Name: "confirm-booking", Status: StepStatusPending},
+		},
 	}
-	o.Steps = append(o.Steps,
-		NewCreateBookingStep(o),
-		NewCreatePaymentStep(o),
-		NewConfirmBookingStep(o),
-	)
-	return o
+	return WithStateMachine(saga, callbacks)
 }
 
-func (o *Orchestrator) CancelBooking() error {
-	return nil
+// WithStateMachine allows the FSM to be attached to each step, especially
+// after deserializing the state.
+func WithStateMachine(saga *Saga, callbacks fsm.Callbacks) *Saga {
+	for i, step := range saga.Steps {
+		saga.Steps[i] = NewCreateBookingStep(step, callbacks)
+		saga.Steps[i] = NewCreatePaymentStep(step, callbacks)
+		saga.Steps[i] = NewConfirmBookingStep(step, callbacks)
+	}
+	return saga
 }
 
-func (o *Orchestrator) CreatePayment() error {
-	// Prepare create payment request.
-	return nil
-}
-
-func (o *Orchestrator) RefundPayment() error {
-	return nil
-}
-
-func (o *Orchestrator) ConfirmBooking() error {
-	return nil
-}
-
-func (o *Orchestrator) RejectBooking() error {
-	return nil
-}
-
-func (o *Orchestrator) On(event string) error {
+func (o *Saga) On(event string) error {
+	var match bool
 	for _, step := range o.Steps {
-		if step.fsm.Can(event) {
-			if err := step.fsm.Event(event); err != nil {
+		if step.FSM.Can(event) {
+			if err := step.FSM.Event(event); err != nil {
 				return err
 			}
-			return nil
+			match = true
 		}
+	}
+	if match {
+		return nil
 	}
 	return fmt.Errorf("invalid event: %s", event)
 }
 
-func (o *Orchestrator) GetStep(name string) (*Step, error) {
+func (o *Saga) GetStep(name string) (*Step, error) {
 	for _, step := range o.Steps {
 		if step.Name == name {
 			return step, nil
@@ -167,25 +227,23 @@ func (o *Orchestrator) GetStep(name string) (*Step, error) {
 	return nil, errors.New("not found")
 }
 
-func (o *Orchestrator) Status() string {
+func (o *Saga) Status() SagaStatus {
+	var compensated int
 	for i, step := range o.Steps {
-		if step.Status == "success" {
+		if step.Status == StepStatusSuccess {
 			if i == len(o.Steps)-1 {
-				return "done"
+				return SagaStatusDone
 			}
 		}
-		if step.Status == "failed" {
-			if i == 0 {
-				return "done"
-			}
-			return "compensating"
+		if step.Status == StepStatusCompensated {
+			compensated++
 		}
-		if step.Status == "compensated" {
-			if i == 0 {
-				return "done"
+		if step.Status == StepStatusFailed {
+			if i == compensated {
+				return SagaStatusDone
 			}
-			return "compensating"
+			return SagaStatusCompensating
 		}
 	}
-	return "pending"
+	return SagaStatusPending
 }
--- go-fsm/main_test.go
+++ go-fsm/main_test.go
@@ -3,12 +3,41 @@ package main
 import (
 	"testing"
 
+	"github.com/looplab/fsm"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestOrchestratorFlow(t *testing.T) {
 	// Given a new orchestrator.
-	o := NewBookingOrchestrator("1")
+
+	var createPayment, confirmBooking, refundPayment, cancelBooking, sagaEnd bool
+	eventHandlers := fsm.Callbacks{
+		"booking_created": func(e *fsm.Event) {
+			t.Logf("%s<%s,%s>\n", e.Event, e.Src, e.Dst)
+			createPayment = true
+		},
+		"payment_created": func(e *fsm.Event) {
+			t.Logf("%s<%s,%s>\n", e.Event, e.Src, e.Dst)
+			confirmBooking = true
+		},
+		"booking_confirmed": func(e *fsm.Event) {
+			t.Logf("%s<%s,%s>\n", e.Event, e.Src, e.Dst)
+			sagaEnd = true
+		},
+		"booking_rejected": func(e *fsm.Event) {
+			t.Logf("%s<%s,%s>\n", e.Event, e.Src, e.Dst)
+			refundPayment = true
+		},
+		"payment_refunded": func(e *fsm.Event) {
+			t.Logf("%s<%s,%s>\n", e.Event, e.Src, e.Dst)
+			cancelBooking = true
+		},
+		"booking_cancelled": func(e *fsm.Event) {
+			t.Logf("%s<%s,%s>\n", e.Event, e.Src, e.Dst)
+			sagaEnd = true
+		},
+	}
+	o := NewBookingSaga("1", eventHandlers)
 
 	// When the booking is created.
 	err := o.On("booking_created")
@@ -19,7 +48,10 @@ func TestOrchestratorFlow(t *testing.T) {
 	// Then the step is completed.
 	step, err := o.GetStep("create-booking")
 	assert.Nil(err)
-	assert.Equal("success", step.Status)
+	assert.Equal(StepStatusSuccess, step.Status)
+
+	// And the create payment command is invoked.
+	assert.True(createPayment)
 
 	// When the payment is created.
 	err = o.On("payment_created")
@@ -28,7 +60,10 @@ func TestOrchestratorFlow(t *testing.T) {
 	// Then the step is completed.
 	step, err = o.GetStep("create-payment")
 	assert.Nil(err)
-	assert.Equal("success", step.Status)
+	assert.Equal(StepStatusSuccess, step.Status)
+
+	// And the confirm booking command is invoked.
+	assert.True(confirmBooking)
 
 	// When the booking is confirmed.
 	err = o.On("booking_confirmed")
@@ -37,10 +72,13 @@ func TestOrchestratorFlow(t *testing.T) {
 	// Then the step is completed.
 	step, err = o.GetStep("confirm-booking")
 	assert.Nil(err)
-	assert.Equal("success", step.Status)
+	assert.Equal(StepStatusSuccess, step.Status)
+
+	// And the saga is completed.
+	assert.True(sagaEnd)
 
 	// And the saga is done.
-	assert.Equal("done", o.Status())
+	assert.Equal(SagaStatusDone, o.Status())
 
 	// When booking rejected.
 	err = o.On("booking_rejected")
@@ -49,10 +87,13 @@ func TestOrchestratorFlow(t *testing.T) {
 	// The the step is marked as failed.
 	step, err = o.GetStep("confirm-booking")
 	assert.Nil(err)
-	assert.Equal("failed", step.Status)
+	assert.Equal(StepStatusFailed, step.Status)
+
+	// And the refund payment command is invoked.
+	assert.True(refundPayment)
 
 	// And the saga is compensating.
-	assert.Equal("compensating", o.Status())
+	assert.Equal(SagaStatusCompensating, o.Status())
 
 	// When payment refunded.
 	err = o.On("payment_refunded")
@@ -61,10 +102,13 @@ func TestOrchestratorFlow(t *testing.T) {
 	// The the step is marked as compensated.
 	step, err = o.GetStep("create-payment")
 	assert.Nil(err)
-	assert.Equal("compensated", step.Status)
+	assert.Equal(StepStatusCompensated, step.Status)
+
+	// And the cancel booking command is invoked.
+	assert.True(cancelBooking)
 
 	// And the saga is compensating.
-	assert.Equal("compensating", o.Status())
+	assert.Equal(SagaStatusCompensating, o.Status())
 
 	// When booking cancelled.
 	err = o.On("booking_cancelled")
@@ -73,8 +117,11 @@ func TestOrchestratorFlow(t *testing.T) {
 	// The the step is marked as compensated.
 	step, err = o.GetStep("create-booking")
 	assert.Nil(err)
-	assert.Equal("compensated", step.Status)
+	assert.Equal(StepStatusCompensated, step.Status)
+
+	// And the saga ends.
+	assert.True(sagaEnd)
 
 	// And the saga is done.
-	assert.Equal("done", o.Status())
+	assert.Equal(SagaStatusDone, o.Status())
 }
